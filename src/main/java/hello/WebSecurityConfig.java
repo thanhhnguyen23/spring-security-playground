63fe6d8c349d895de6f7514878c83ef8c4bd3871
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2019-11-13 22:00:36 -0600

    HTTP Basic security example

--- src/main/java/hello/WebSecurityConfig.java
+++ src/main/java/hello/WebSecurityConfig.java
@@ -1,13 +1,17 @@
 package hello;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+// @Configuration
+// @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
